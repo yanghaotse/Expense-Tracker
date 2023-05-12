feb644d5680e62f0e6c7a3ec04fb5225a6b1fe3d
yang haotse <ha91252@gmail.com>
2023-05-12 12:45:54 +0800

    feat: add mongoDB connection & Models folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
@@ -11,6 +11,20 @@ app.set('view engine', 'hbs')
 
 // body-parser
 app.use(express.urlencoded({extended: true}))
+
+// mongoDB
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongoDB error')
+})
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
 
 
 
