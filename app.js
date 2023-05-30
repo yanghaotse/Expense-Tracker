8a7a8d5f14299d1e7b19ca9d73ebb9ce524316d3
yang haotse <ha91252@gmail.com>
2023-05-30 20:59:00 +0800

    feat: add 'route:/records/delete' && 'total cost feature'

--- app.js
+++ app.js
@@ -42,27 +42,40 @@ app.get('/', async (req, res) => {
     const records = await RecordModel.find().populate('categoryId').lean()
     const data = records.map(record => {
       const {_id, name, date, amount} = record
+      // date轉換格式為: YYYY/MM/DD
+      const formatDate = new Date(date).toLocaleDateString('zh-TW', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      })
       return{
         _id,
         name,
-        date,
+        date: formatDate,
         amount,
         icon: record.categoryId.icon
       }
     })
+    // 計算總金額
+    const totalCost = data.reduce(((accumulator, item) => {
+      return accumulator + item.amount
+    }), 0)
+    
+    
+    console.log(totalCost)
     // console.log(data) //檢查用
-    res.render('index', {records: data})
+    res.render('index', { records: data , totalCost})
   }catch(err){
     console.log(err)
   }
 })
 
-
+// route: GET/new
 app.get('/records/new',(req, res) => {
   res.render('new')
 })
 
-//編輯一筆資料GET
+// route: GET/edit
 app.get('/records/edit/:_id',async (req, res) => {
   const _id = req.params._id
   // console.log(id) //檢查用
@@ -76,13 +89,13 @@ app.get('/records/edit/:_id',async (req, res) => {
 })
 
 
-// 編輯一筆資料POST
+// route: POST/edit
 app.post('/records/edit/:_id', async(req, res) => {
   const _id = req.params._id
   const  {name, date, category, amount} = req.body
   try{
-    // 抓取RecordModel資料若後面有家lean()，之後在儲存資料.save()時會因為資料格式問題無法儲存
     // const record = await RecordModel.findOne({ _id }).lean()
+    // 抓取RecordModel資料若後面有家lean()，之後用.save()儲存資料時會因為資料格式問題無法儲存，改用.updateOne
     const categoryData = await CategoryModel.findOne({ name : category}).lean()
     const record = {
       name,
@@ -98,7 +111,7 @@ app.post('/records/edit/:_id', async(req, res) => {
   }
 })
 
-// 新增一筆資料
+// route: POST/new
 app.post('/records/new', async(req, res) => {
   const {name, date, category, amount} = req.body
   // console.log(name,date, category, amount) //檢查用
@@ -114,7 +127,16 @@ app.post('/records/new', async(req, res) => {
   }catch(err){
     console.log(err)
   }
-  
+})
+
+app.post('/records/delete/:_id', async(req, res) => {
+  const _id = req.params._id
+  try{
+    await RecordModel.findByIdAndDelete(_id)
+    res.redirect('/')
+  }catch(err){
+    console.log(err)
+  }
 })
 
 
