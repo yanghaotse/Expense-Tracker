b6846487eca0a2a1d08320a9837d0329db97939a
yang haotse <ha91252@gmail.com>
2023-06-14 11:15:57 +0800

    Update code

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const moment = require('moment')
 const flash = require('connect-flash')
 
-const RecordModel = require('./models/record')
-const CategoryModel = require('./models/category')
 const usePassport = require('./config/passport')
-
 const routes = require('./routes')
 require('./config/mongoose') // mongoDB
 
 const app = express()
-
+const PORT = process.env.PORT || 3000
 
 
 //express-handlebars
@@ -21,6 +17,7 @@ app.engine('hbs', exphbs({ defaultLayout : 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs') 
 
 // express-session
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -53,7 +50,7 @@ app.use(routes)
 
 
 app.listen( process.env.PORT, () => {
-  console.log(`App is running on http://localhost:${process.env.PORT}`)
+  console.log(`App is running on http://localhost:${PORT}`)
 })
 
 
