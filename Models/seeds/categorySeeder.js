f9b886242fca1cd46039f3693ad6cf1b53b55934
yang haotse <ha91252@gmail.com>
2023-07-11 11:48:22 +0800

    edit categorySeeder.js

--- Models/seeds/categorySeeder.js
+++ Models/seeds/categorySeeder.js
@@ -7,28 +7,20 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 
-
 db.once('open', async () => {
   try {
+    const categoryModel = await CategoryModel.find().lean()
     // Model.create()可以直接傳進一陣列
-    await CategoryModel.create(SEED_CATEGORY);
-    console.log('所有類別創建完成');
+    if(categoryModel.length == 0){
+      await CategoryModel.create(SEED_CATEGORY);
+      console.log('所有類別創建完成');
+    }
     process.exit();
-  } catch (err) {
+  }catch (err) {
     console.log(err);
   }
 });
 
-// db.once('open', async() => {
-//   try {
-//     await Promise.all(
-//       SEED_CATEGORY.map(async seedCategory => {Model
-//       await CategoryModel.create(seedCategory)
-//       })
-//     )
-//     console.log('category created')
-//     process.exit()
-//   }catch(err){
-//     console.log(err)
-//   }
-// })
+
+
+  
